adbfb8ae1a4a5357139afde7c4e3e7435a6f48b5
Daria bc2025-3 <kozikovadasa@gmail.com>
2025-10-05 12:43:31 +0300

    Fixed main.js

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const { program } = require('commander');
 const fs = require('fs');
 const path = require('path');
 
-
 program
-  .requiredOption('-i, --input <file>', 'Path to input JSON file')
+  .option('-i, --input <file>', 'Path to input JSON file')
   .option('-o, --output <file>', 'Path to output file')
   .option('-d, --display', 'Display result in console')
   .option('-h, --humidity', 'Show humidity (Humidity3pm)')
@@ -13,12 +12,15 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
 
 if (!fs.existsSync(options.input)) {
   console.error("Cannot find input file");
   process.exit(1);
 }
-
 
 let data;
 try {
@@ -33,7 +35,6 @@ try {
   process.exit(1);
 }
 
-
 if (!Array.isArray(data)) {
   console.error("Input JSON should be an array of records");
   process.exit(1);
@@ -42,18 +43,16 @@ if (!Array.isArray(data)) {
 const outputLines = [];
 
 data.forEach((record) => {
-
   if (options.rainfall !== undefined && (record.Rainfall === undefined || record.Rainfall <= options.rainfall)) {
-    return; 
+    return;
   }
 
- 
   const parts = [];
-  parts.push(record.Rainfall !== undefined ? record.Rainfall : 0);
-  parts.push(record.Pressure3pm !== undefined ? record.Pressure3pm : 0);
+  parts.push(record.Rainfall ?? 0);
+  parts.push(record.Pressure3pm ?? 0);
 
   if (options.humidity) {
-    parts.push(record.Humidity3pm !== undefined ? record.Humidity3pm : 0);
+    parts.push(record.Humidity3pm ?? 0);
   }
 
   outputLines.push(parts.join(' '));
@@ -61,16 +60,14 @@ data.forEach((record) => {
 
 const output = outputLines.join('\n');
 
-
 if (options.display) {
   console.log(output);
 }
 
-
 if (options.output) {
   try {
     fs.writeFileSync(path.resolve(options.output), output, 'utf-8');
-  } catch (err) {
+  } catch {
     console.error("Error writing to output file");
   }
 }
